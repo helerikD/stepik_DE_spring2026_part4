f458f1dea5164d8f2c992888c88b033b73a10b67
helerikD <myemail@mail.ru>
2026-04-12 23:48:52 +0300

    added some comment in py file of user2

--- sailing_distance.py
+++ sailing_distance.py
@@ -1,17 +1,17 @@
 # Вычисление пройденного судном пути: расстояние (нм) = скорость (уз) * время (ч).
-while True:
-    s = input("Введите скорость в узлах или нажмите Enter для выхода:\n").strip()
-    if s == "":
-        break
-    t = input("Введите время в часах:\n").strip()
-    try:
-        speed = float(s)
-        time = float(t)
-        if speed < 0 or time < 0:
-            print("Не может быть отрицательным числом")
-            continue
-    except ValueError:
-        print("Неверный ввод. Введите число")
-        continue
-    distance = speed * time
-    print(f"Пройденное расстояние: {distance:.2f} нм\n")
+while True: #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+    s = input("Введите скорость в узлах или нажмите Enter для выхода:\n").strip() #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+    if s == "": #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+        break #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+    t = input("Введите время в часах:\n").strip() #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+    try: #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+        speed = float(s) #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+        time = float(t) #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+        if speed < 0 or time < 0: #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+            print("Не может быть отрицательным числом") #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+            continue #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+    except ValueError: #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+        print("Неверный ввод. Введите число") #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+        continue #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+    distance = speed * time #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
+    print(f"Пройденное расстояние: {distance:.2f} нм\n") #some comment b05c8f24-ea89-4c03-bcfc-be0ed16d8b3c
