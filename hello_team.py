5466bb25bac796d177f348283fed419ab1cba391
helerikD <email123984646123894@email129047451891274.ru>
2026-04-13 00:10:39 +0300

    my changes in py file of user1 waiting changes of that file by user2

--- hello_team.py
+++ hello_team.py
@@ -1,5 +1,5 @@
-team_members = []
-with open('README.MD', "r", encoding='utf8') as f:
-    for line in f.readlines():
-        if line[0] == '@':
-            print(f'Hello, Dear teammate {line[1:].strip('\n')}!')
+team_members = [] #d453fbe1-5bd8-4ce3-a358-48970a6672f5
+with open('README.MD', "r", encoding='utf8') as f: #d453fbe1-5bd8-4ce3-a358-48970a6672f5
+    for line in f.readlines(): #d453fbe1-5bd8-4ce3-a358-48970a6672f5
+        if line[0] == '@': #d453fbe1-5bd8-4ce3-a358-48970a6672f5
+            print(f'Hello, Dear teammate {line[1:].strip('\n')}!') #d453fbe1-5bd8-4ce3-a358-48970a6672f5
